646bd83747bf320a37faf660e4d076b31d85ce27
Kemboi Elvis <kemboielvis22@gmail.com>
2026-05-10 02:31:00 +0300

    deps: update to v0.80

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="docmost"
-APP_VERSION="0.70.1"
+APP_VERSION="0.80"
 
 # Create a new empty container image
 container=$(buildah from scratch)
